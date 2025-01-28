e83622d8cbd8d63f1d1830ccf837a30391278be4
Your Name <ozodjonovzikirllo@gmail.com>
2025-01-29 01:59:35 +0800

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+
+
 void main() {
-  runApp(
-    MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: const WebViewApp(),
-    ),
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
 }
 
-class WebViewApp extends StatefulWidget {
-  const WebViewApp({super.key});
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
-  State<WebViewApp> createState() => _WebViewAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BORGO',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const WebViewExample(),
+    );
+  }
 }
 
-class _WebViewAppState extends State<WebViewApp> {
-  late final WebViewController controller;
+class WebViewExample extends StatefulWidget {
+  const WebViewExample({super.key});
+
+  @override
+  State<WebViewExample> createState() => _WebViewExampleState();
+}
+
+class _WebViewExampleState extends State<WebViewExample> {
+  late WebViewController controller;
+  var loadingPercentage = 0;
 
   @override
   void initState() {
     super.initState();
     controller = WebViewController()
-      ..loadRequest(
-        Uri.parse('https://borgo.uz'),
-      );
+      ..setNavigationDelegate(NavigationDelegate(onPageStarted: (url) {
+        loadingPercentage = 0;
+      }, onProgress: (progress) {
+        setState(() {
+          loadingPercentage = progress;
+        });
+      }, onPageFinished: (url) {
+        setState(() {
+          loadingPercentage = 100;
+        });
+      }))
+      ..loadRequest(Uri.parse("https://borgo.uz"));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter WebView'),
-      ),
-      body: WebViewWidget(
-        controller: controller,
+      body: Stack(
+        children: [
+          WebViewWidget(controller: controller),
+          loadingPercentage < 100 ?
+            LinearProgressIndicator(
+              color: Colors.green,
+              value: loadingPercentage/100,
+            ) : Container()
+        ],
       ),
     );
   }
